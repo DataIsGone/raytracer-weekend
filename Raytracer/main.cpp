a302815005585f0e0c325b7c50b569079e915ee1
Ryan Davis <ryan.alex.davis@gmail.com>
2023-09-15 13:01:41 -0700

    Starting up raytracer tutorial

--- Raytracer/main.cpp
+++ Raytracer/main.cpp
@@ -1,14 +1,25 @@
-//
-//  main.cpp
-//  Raytracer
-//
-//  Created by Ryan Davis on 9/15/23.
-//
-
 #include <iostream>
+using namespace std
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    // Image window
+    int image_width = 256;
+    int image_height = 256;
+    
+    // Render
+    cout << "P3\n" << image_width << ' ' <<image_height << "\n255\n";
+    
+    for (int j = 0; j < image_height; ++j) {
+        for (int i = 0; i < image_width; ++i) {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0;
+            
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.99 * g);
+            int ib = static_cast<int>(255.999 * b);
+            
+            cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
 }
